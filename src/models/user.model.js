6cfbcddbfdc06d13ea58ebaa4c1033e4570a5827
Lipika-M <lipikaam08@gmail.com>
2026-04-02 21:50:53 +0530

    Refactor password hashing in user model to simplify pre-save middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,14 +53,9 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
-  try {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (err) {
-    next(err);
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
