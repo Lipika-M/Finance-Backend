5f1927dbbf2937b053ef82d06bbf579e43dff671
Lipika-M <lipikaam08@gmail.com>
2026-04-02 15:42:15 +0530

    Set up project structure with Prettier configuration, MongoDB connection, and initial Express server

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+ 
+
+const connectDB = async () => {
+  try {
+    const connection_instance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Connected to MongoDB !! ",connection_instance.connection.host);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
