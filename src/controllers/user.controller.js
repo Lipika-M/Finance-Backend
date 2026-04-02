5d8ab00dc8ffb90bea8ab10dc0ee24353f2ca9b3
Lipika-M <lipikaam08@gmail.com>
2026-04-02 21:37:49 +0530

    Fix import path for User model and improve error handling in index.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
-import { User } from "../models/User.js";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 const ensureAdmin = (req) => {
